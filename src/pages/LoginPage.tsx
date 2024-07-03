05e6322b4d364ffa0d2c4452d6128474a1246b81
akhil-haridas <4khilharidas@gmail.com>
2024-07-03 16:39:12 +0530

    added login function

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { supabase } from "../supabase";
 
 const LoginPage = () => {
-    return (
+    const signInWithGoogle = async () => {
+        const { error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+        })
 
+        if (error) console.error("Error signing in with Google: ", error.message);
+    };
+
+    return (
         <div className="container flex flex-col mx-auto bg-white rounded-lg pt-12 my-5">
             <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
                 <div className="flex items-center justify-center w-full lg:p-12">
@@ -11,14 +19,18 @@ const LoginPage = () => {
                             <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">
                                 Sign In
                             </h3>
-                            <a className="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
+                            <button
+                                type="button"
+                                onClick={signInWithGoogle}
+                                className="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300"
+                            >
                                 <img
                                     className="h-5 mr-2"
                                     src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
-                                    alt=""
+                                    alt="Google Logo"
                                 />
                                 Sign in with Google
-                            </a>
+                            </button>
                         </form>
                     </div>
                 </div>
