e049437957a2a1b96d4bf9ae22f2f3e8b73ad59e
akhil-haridas <4khilharidas@gmail.com>
2024-07-03 11:00:18 +0530

    added chat  component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as AppLogo } from './Logo';
 export { default as Profile } from './Profile';
-export { default as ChatList } from './Chatlist';
+export { default as Users } from './Users';
+export { default as ChatMessages } from './Chat';
