0fcb22f1a49a47baa42dd0101b34e435104d297d
akhil-haridas <4khilharidas@gmail.com>
2024-07-03 11:01:32 +0530

    added message component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as AppLogo } from './Logo';
 export { default as Profile } from './Profile';
 export { default as Users } from './Users';
 export { default as ChatMessages } from './Chat';
+export { default as Message } from './Message';
