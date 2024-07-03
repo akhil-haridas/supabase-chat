ba939a395cc210257f3de974f2e0c812ef26eab9
akhil-haridas <4khilharidas@gmail.com>
2024-07-03 16:41:11 +0530

    fix attribute typo

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -18,7 +18,7 @@ const Message = () => {
                     >
                         <path
                             strokeLinecap="round"
-                            stroke-linejoin="round"
+                            strokeLinejoin="round"
                             strokeWidth="2"
                             d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
                         ></path>
@@ -43,7 +43,7 @@ const Message = () => {
                         >
                             <path
                                 strokeLinecap="round"
-                                stroke-linejoin="round"
+                                strokeLinejoin="round"
                                 strokeWidth="2"
                                 d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                             ></path>
@@ -66,7 +66,7 @@ const Message = () => {
                         >
                             <path
                                 strokeLinecap="round"
-                                stroke-linejoin="round"
+                                strokeLinejoin="round"
                                 strokeWidth="2"
                                 d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
                             ></path>
