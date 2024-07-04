5dd20fd1523b28ff75db9e32c8a81f99753cd15b
akhil-haridas <4khilharidas@gmail.com>
2024-07-04 11:49:16 +0530

    user card listing

--- src/components/utils/User.tsx
+++ src/components/utils/User.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-const User = () => {
+interface UserProps {
+    user: {
+        name: string;
+        picture: string;
+    };
+}
+
+const User: React.FC<UserProps> = ({ user }) => {
     return (
         <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
             <div className="flex items-center justify-center h-8 w-8 bg-gray-200 rounded-full">
-                M
+                <img
+                    src={user.picture}
+                    alt="User Avatar"
+                    className="h-full w-full rounded-full"
+                />
             </div>
-            <div className="ml-2 text-sm font-semibold">Marta Curtis</div>
+            <div className="ml-2 text-sm font-semibold">{user.name}</div>
             <div className="flex items-center justify-center ml-auto text-xs text-white bg-red-500 h-4 w-4 rounded leading-none">
-                { true && 2}
+                {true && 2}
             </div>
         </button>
     );
