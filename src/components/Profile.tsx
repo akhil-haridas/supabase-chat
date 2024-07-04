498f812e85d8661fabbe0c9296aaa5afd8e6759d
akhil-haridas <akhil.k@dignizant.com>
2024-07-04 10:51:31 +0530

    using profile data and logout functionality

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import { supabase } from "../supabase";
 
 interface ProfileProps {
     user: {
@@ -9,29 +10,29 @@ interface ProfileProps {
 }
 
 const Profile: React.FC<ProfileProps> = ({ user }) => {
+    const handleSignoutUser = async (e: any) => {
+        e.preventDefault();
+        const { error } = await supabase.auth.signOut();
+        console.log(error);
+    };
+
     return (
-        <div
-            className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg"
-        >
+        <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
             <div className="h-20 w-20 rounded-full border overflow-hidden">
-                <img
-                    src={user.picture}
-                    alt="Avatar"
-                    className="h-full w-full"
-                />
+                <img src={user.picture} alt="Avatar" className="h-full w-full" />
             </div>
             <div className="text-sm font-semibold mt-2">{user.name}</div>
             <div className="text-xs text-gray-500">{user.email}</div>
             <div className="flex flex-row items-center mt-3">
-                <div
-                    className="flex flex-col justify-center h-4 w-8 bg-indigo-500 rounded-full"
-                >
+                <div className="flex flex-col justify-center h-4 w-8 bg-indigo-500 rounded-full">
                     <div className="h-3 w-3 bg-white rounded-full self-end mr-1"></div>
                 </div>
-                <div className="leading-none ml-1 text-xs">Active</div>
+                <div className="leading-none ml-1 text-xs" onClick={handleSignoutUser}>
+                    Active
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Profile
+export default Profile;
