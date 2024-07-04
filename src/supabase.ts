1fccd4487643f6c8b83add6555d5f542a9bdea07
akhil-haridas <akhil.k@dignizant.com>
2024-07-04 10:51:12 +0530

    using supabase key as env secret

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ccsmjjmnwgfyrljpvqrc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjc21qam1ud2dmeXJsanB2cXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5ODE0ODgsImV4cCI6MjAzNTU1NzQ4OH0.1qVb_yNNCD7fP1dAYebZVEH7fV46AW3YMumJ5FKFJ2s';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
