d1261a1c867e6bbf38fa5d39e38ac79dbac226b1
akhil-haridas <4khilharidas@gmail.com>
2024-07-04 16:02:30 +0530

    get Message function

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import { TextContent, VoiceContent } from './utils';
+import { Suspense, useEffect } from "react";
+import { TextContent, VoiceContent } from "./utils";
+import { Loading } from "../pages";
+import { supabaseClient } from "../supabase/supabaseClient";
 
 const ChatMessages = () => {
+    useEffect(() => {
+        const getMessages = async () => {
+            const { data } = await supabaseClient.from("messages").select("*");
+            console.log("DATA :", data);
+        };
+        getMessages();
+    }, []);
+
     return (
-        <div className="flex flex-col h-full overflow-x-auto mb-4">
-            <div className="flex flex-col h-full">
-                <div className="grid grid-cols-12 gap-y-2">
-                    <TextContent currentUser={false} />
-                    <TextContent currentUser={true} />
-                    <VoiceContent/>
+        <Suspense fallback={<Loading />}>
+            <div className="flex flex-col h-full overflow-x-auto mb-4">
+                <div className="flex flex-col h-full">
+                    <div className="grid grid-cols-12 gap-y-2">
+                        <TextContent currentUser={false} />
+                        <TextContent currentUser={true} />
+                        <VoiceContent />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        </Suspense>
+    );
+};
 
 export default ChatMessages;
