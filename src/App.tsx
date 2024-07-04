85cb7bf5bfe37277c9cd125176d1e50f226aef60
akhil-haridas <akhil.k@dignizant.com>
2024-07-04 10:51:56 +0530

    update app.tsx with session and user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,64 @@
 import { useEffect, useState } from "react";
-
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from "react-router-dom";
-import { ChatPage, LoginPage } from "./pages";
+import { ChatPage, Loading, LoginPage } from "./pages";
 import { supabase } from "./supabase";
 
 const App = () => {
   const [session, setSession] = useState<any>(null);
   const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
+    const getSession = async () => {
+      const {
+        data: { session },
+        error: sessionError,
+      } = await supabase.auth.getSession();
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+
+      if (sessionError || userError) {
+        console.error(
+          "Error fetching session or user:",
+          sessionError || userError
+        );
+        setLoading(false);
+        return;
+      }
+
       setSession(session);
-      if (session) setUser(session?.user?.user_metadata);
-    });
+      if (session) {
+        setUser(session?.user?.user_metadata);
+      } else {
+        setUser(user?.user_metadata);
+      }
+      setLoading(false);
+    };
+
+    getSession();
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
+      if (session) {
+        setUser(session?.user?.user_metadata);
+      } else {
+        setUser(null);
+      }
     });
 
     return () => subscription.unsubscribe();
   }, []);
+
+  if (loading) return <Loading />;
 
   return (
     <Router>
@@ -37,13 +69,13 @@ const App = () => {
             session && user ? (
               <ChatPage user={user} />
             ) : (
-              <Navigate to={"/login"} />
+              <Navigate to="/login" />
             )
           }
         />
         <Route
           path="/login"
-          element={session && user ? <Navigate to={"/"} /> : <LoginPage />}
+          element={session && user ? <Navigate to="/" /> : <LoginPage />}
         />
       </Routes>
     </Router>
