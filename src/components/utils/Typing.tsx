96ce6f328ab5377b3a955ea3dab4d63700d46983
akhil-haridas <4khilharidas@gmail.com>
2024-07-05 17:14:35 +0530

    component for the typing

---
+++ src/components/utils/Typing.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Typing = ({ user }) => {
+    return (
+        <div className="col-start-1 col-end-8 p-6 rounded-lg">
+            <div className="flex flex-row items-center">
+                <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                    <img
+                        src={user?.picture}
+                        alt="A"
+                        className="h-full w-full rounded-full"
+                    />
+
+                </div>
+                <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                    <div className="absolute text-xs -top-1 left-0 min-w-500 -mt-4 font-bold mr-2 text-gray-500">
+                        {user?.name}
+                    </div>
+                    <div>{user?.message}</div>
+                    <div className="absolute text-xs bottom-0 min-w-44  -mb-5 mr-2 text-gray-500">
+                        {/* {formatTime(new Date(content.created_at))} */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Typing
--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as TextContent } from './TextContent';
 export { default as VoiceContent } from './VoiceContent';
 export { default as UserButton } from './User';
+export { default as Typing } from './Typing';
