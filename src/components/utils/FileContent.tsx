8d7a6920c14b5f2aaf5ff440c1cc33c902d1ebb4
akhil-haridas <4khilharidas@gmail.com>
2024-07-09 15:46:22 +0530

    commented unused code

--- src/components/utils/FileContent.tsx
+++ src/components/utils/FileContent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { supabaseClient } from "../../supabase/supabaseClient";
+// import { supabaseClient } from "../../supabase/supabaseClient";
 
 interface ContentProps {
     currentUser: boolean;
